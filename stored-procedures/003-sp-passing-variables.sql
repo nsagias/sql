c437234bece55b2459ab23d3b3b25123bbb62051
Nick <nsagias@gmail.com>
2022-12-03 18:04:57 -0500

    add passing variables sqrt and add two nubmer

---
+++ stored-procedures/003-sp-passing-variables.sql
@@ -0,0 +1,23 @@
+DELIMITER $$
+DROP PROCEDURE IF EXISTS sp_square_root$$
+CREATE PROCEDURE sp_square_root(inputNumber INT)
+BEGIN
+	DECLARE l_sqrt FLOAT;
+    SET l_sqrt=SQRT(inputNumber);
+    SELECT l_sqrt;
+END$$
+DELIMITER ;
+CALL sp_square_root(144);
+
+DELIMITER $$
+
+DROP PROCEDURE IF EXISTS sp_add_two_numbers$$
+CREATE PROCEDURE sp_add_two_numbers(firstNumber INT, secondNumber INT)
+BEGIN
+	DECLARE result INT;
+    SET result=firstNumber + secondNumber;
+    SELECT result;
+END$$
+DELIMITER ;
+
+CALL sp_add_two_numbers(1, 3);
