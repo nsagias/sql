07465ad15dea4e1933321614e4cc76b16e647171
Nick <nsagias@gmail.com>
2022-12-03 20:53:57 -0500

    basic loop example

---
+++ stored-procedures/005-sp-basic-loops.sql
@@ -0,0 +1,18 @@
+DELIMITER $$
+
+DROP PROCEDURE IF EXISTS basic_loop$$
+CREATE PROCEDURE basic_loop()
+BEGIN
+	DECLARE counter INT 
+		DEFAULT 0;
+    basic_loop_name:LOOP
+		SET counter=counter +1;
+        IF counter=10 THEN
+			LEAVE basic_loop_name;
+		END IF;
+    END LOOP basic_loop_name;
+    SELECT 'COUNTING TO TEN';
+END $$
+
+DELIMITER ;
+CALL basic_loop();
